9569c4763a3bf7d3603a4c996e39295bfb0d2b2c
Alisson Bergamin <alisson.bergs@gmail.com>
2025-05-19 20:48:40 -0300

    Project created

---
+++ src/test/java/com/bergamo/berglist/BerglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bergamo.berglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
